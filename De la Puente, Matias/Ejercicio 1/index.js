f58f32549efb20fdc37525563976ad934bf07410
Alaniz Martin <alanizmartin001@gmail.com>
2025-09-05 03:45:33 -0300

    Ejercicio 1 finalizado

---
+++ De la Puente, Matias/Ejercicio 1/index.js
@@ -0,0 +1,61 @@
+import express from 'express'
+
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+const calculos = []
+let nextId = 1
+
+app.post('/calcular', (req, res) => {
+  let { opcion, base, altura } = req.body   
+
+  opcion = opcion.toLowerCase()
+
+  if (opcion !== 'area' && opcion !== 'perimetro') {
+    return res.status(400).json({ error: 'Opción inválida, debe ser "area" o "perimetro"' })
+  }
+
+  if (!base || !altura || base <= 0 || altura <= 0) {
+    return res.status(400).json({ error: 'Base y altura deben ser números positivos' })
+  }
+
+  let resultado
+  if (opcion === 'area') {
+    resultado = base * altura
+  } else {
+    resultado = 2 * (base + altura)
+  }
+
+  const newCalculo = { id: nextId++, opcion, base, altura, resultado }
+  calculos.push(newCalculo)
+
+  res.json(newCalculo)
+})
+
+
+app.get('/calculos', (req, res) => {
+  if (calculos.length === 0) {
+    return res.json({ message: 'No hay cálculos realizados aún.' })
+  }
+  const formaResultados = calculos.map(c =>{
+    const tipo = c.base === c.altura ? 'cuadrado' : 'rectángulo'
+    return { ...c, tipo }
+  })
+  res.json({ calculos: formaResultados})
+})
+
+app.delete('/calculos/:id', (req, res) => {
+  const id = parseInt(req.params.id)
+  if (isNaN(id) || id < 1 || id > calculos.length) {
+    return res.status(400).json({ error: 'ID inválido' })
+  }
+  const index = calculos.find(c => c.id === id)
+  const calculoEliminado = calculos.splice(index, 1)
+  res.json({ message: 'Cálculo eliminado', calculo: calculoEliminado})
+})
+
+app.listen(port, () => {
+  console.log(`La aplicación está funcionando en http://localhost:${port}`)
+})
